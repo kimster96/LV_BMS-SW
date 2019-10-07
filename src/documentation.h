25eb2f571fb62f0b7faf821bd673479ae25f5fc6
jagjordi <jordialtayo@gmail.com>
2019-10-07 16:47:37 +0200

    doc changes

--- src/documentation.h
+++ src/documentation.h
@@ -1,8 +1,8 @@
 // this file is only used for automatic documentation porpuses 
 
 /** 
-* \mainpage
-* 
-* \title LV BMS Softwareeeee
-* \author Jordi Altayó
-*/
+ * \mainpage
+ * 
+ * \title LV BMS Softwareeeee
+ * \author Jordi Altayó
+ */
