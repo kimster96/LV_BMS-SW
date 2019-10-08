736ce4b20003077fac67e35d1708abccc4abf333
jagjordi <jordialtayo@gmail.com>
2019-10-08 18:39:28 +0200

    deleted documentation file

--- src/documentation.h
+++
@@ -1,9 +0,0 @@
-// this file is only used for automatic documentation porpuses 
-
-/** 
- * \mainpage
- * 
- * This is an introduction
- * \title LV BMS Softwareeeee
- * \author Jordi Altayó
- */
--- src/main.c
+++ src/main.c
@@ -1,3 +1,13 @@
+/**
+ * \mainpage
+ *
+ * \author Jordi Altay&oacute;
+ *
+ * This is the main page of this
+ *
+ */
+
+
 #include <asf.h>
 
 int main (void)
