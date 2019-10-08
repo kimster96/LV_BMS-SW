0d369781814bde0dcf986b9ec471aa04430c1dd2
jagjordi <jordialtayo@gmail.com>
2019-10-08 23:07:12 +0200

    doxu

--- src/main.c
+++ src/main.c
@@ -7,7 +7,18 @@
  * 
  * \section requirements Requirements
  * 
- * some requirements bla bla bla
+ * The LV BMS has a set of requirements imposed by both the FSG rules and internal requirements. The summary of requirements is lested as follows
+ *
+ * FSG Rules requirements:
+ * \li Monitor all cell voltages
+ * \li Monitor >30% of cell temperatures
+ * \li Monitor the output current
+ * \li Transmit the above measurements through CAN
+ * \li Interrupt the output if the voltages exceed the limits for more than 500 ms
+ * \li Interrupt the output if the temperatures exceed the limits for more than 1 s
+ * Internal requirements:
+ * \li Configurable thresholds via CAN messages
+ * \li Store thresholds on non volatile memory and retreive the on power-up
  *
  * \subsection rules FSG Rules
  * 
