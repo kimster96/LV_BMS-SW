33b5322a8ababe9037e339255871a69774140962
jagjordi <jordialtayo@gmail.com>
2019-10-08 18:51:36 +0200

    added task diagram

--- src/main.c
+++ src/main.c
@@ -4,6 +4,10 @@
  * \author Jordi Altay&oacute;
  *
  * This is the main page of this
+ * 
+ * \section RTOS structure
+ * Here a small diagram
+ * \image html task_diagram.png "Task diagram"
  *
  */
 
