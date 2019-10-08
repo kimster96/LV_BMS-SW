c5b7ceea5ad6e271d1ad254765173b5bf348c635
jagjordi <jordialtayo@gmail.com>
2019-10-08 22:24:02 +0200

    documentation changes

--- src/main.c
+++ src/main.c
@@ -1,10 +1,14 @@
 /**
  * \mainpage
  *
- * \author Jordi Altay&oacute;
+ * This is the main page of the LV BMS software documentation.
  *
- * This is the main page of this
+ * \section 
+ * \tableofcontents
  * 
+ * \section Requirements
+ * some requirements bla bla bla
+ * \subsection FSG Rules
  * \section RTOS structure
  *
  * Here a small diagram
